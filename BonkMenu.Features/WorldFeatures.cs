76a52bfc7bac136110e6dca7369e8ce2d8f523a9
Goodzy <bonkmenu@goodzy.dev>
2025-11-26 10:07:14 +1100

    Fix encounter spawning to create physical world objects instead of just UI screens

--- BonkMenu.Features/WorldFeatures.cs
+++ BonkMenu.Features/WorldFeatures.cs
@@ -3,6 +3,7 @@ using Il2CppAssets.Scripts.Actors.Player;
 using Il2CppAssets.Scripts.UI.InGame.Rewards;
 using Il2CppAssets.Scripts.UI.InGame.Levelup;
 using Il2CppAssets.Scripts.Inventory__Items__Pickups.Interactables;
+using Il2CppAssets.Scripts.Inventory__Items__Pickups.Chests;
 using MelonLoader;
 using UnityEngine;
 
@@ -52,23 +53,73 @@ public static class WorldFeatures
 
 	public static void SpawnEncounter(int encounterId, string encounterName)
 	{
-		MelonLogger.Msg($"[WorldFeatures] Spawning encounter: {encounterName} (ID: {encounterId})");
+		MelonLogger.Msg($"[WorldFeatures] Attempting to spawn physical encounter: {encounterName} (ID: {encounterId})");
 		try
 		{
-			EncounterWindows encounterWindows = Object.FindObjectOfType<EncounterWindows>();
-			if ((Object)(object)encounterWindows == (Object)null)
+			MyPlayer player = Object.FindObjectOfType<MyPlayer>();
+			if ((Object)(object)player == (Object)null)
 			{
-				MelonLogger.Warning("[WorldFeatures] EncounterWindows not found - are you in a run?");
+				MelonLogger.Warning("[WorldFeatures] Player not found - are you in a run?");
 				return;
 			}
 
-			EEncounter encounter = (EEncounter)encounterId;
-			encounterWindows.AddEncounter(encounter);
-			MelonLogger.Msg($"[WorldFeatures] Successfully spawned encounter: {encounterName}");
+			// Calculate spawn position in front of player
+			Vector3 spawnPosition = player.transform.position + player.transform.forward * 5f;
+			spawnPosition.y = player.transform.position.y; // Keep at player height
+
+			// Try to find existing chest in scene to use as prefab
+			InteractableChest[] existingChests = Object.FindObjectsOfType<InteractableChest>();
+			GameObject chestPrefab = null;
+
+			if (existingChests.Length > 0)
+			{
+				chestPrefab = existingChests[0].gameObject;
+				MelonLogger.Msg($"[WorldFeatures] Found existing chest to use as template");
+			}
+
+			// Spawn based on encounter type
+			GameObject spawnedObject = null;
+			switch (encounterId)
+			{
+				case 3: // ChestNormal
+				case 4: // ChestFree
+				case 5: // ChestEvil
+					if ((Object)(object)chestPrefab != (Object)null)
+					{
+						spawnedObject = Object.Instantiate(chestPrefab, spawnPosition, Quaternion.identity);
+						var chestComponent = spawnedObject.GetComponent<InteractableChest>();
+						if ((Object)(object)chestComponent != (Object)null)
+						{
+							// Set chest type based on encounter
+							EChest chestType = encounterId == 3 ? (EChest)0 : (encounterId == 4 ? (EChest)2 : (EChest)1);
+							chestComponent.chestType = chestType;
+							string typeName = encounterId == 3 ? "normal" : (encounterId == 4 ? "free" : "corrupt");
+							MelonLogger.Msg($"[WorldFeatures] Spawned {typeName} chest at {spawnPosition}");
+						}
+					}
+					else
+					{
+						MelonLogger.Warning("[WorldFeatures] No chest prefab found - cannot spawn physical chest");
+					}
+					break;
+
+				default:
+					MelonLogger.Warning($"[WorldFeatures] Encounter type {encounterName} cannot be spawned as physical object yet");
+					// Fall back to UI encounter for unsupported types
+					EncounterWindows encounterWindows = Object.FindObjectOfType<EncounterWindows>();
+					if ((Object)(object)encounterWindows != (Object)null)
+					{
+						EEncounter encounter = (EEncounter)encounterId;
+						encounterWindows.AddEncounter(encounter);
+						MelonLogger.Msg($"[WorldFeatures] Triggered UI encounter instead: {encounterName}");
+					}
+					break;
+			}
 		}
 		catch (Exception ex)
 		{
 			MelonLogger.Error($"[WorldFeatures] Failed to spawn encounter {encounterName}: {ex.Message}");
+			MelonLogger.Error($"[WorldFeatures] Stack trace: {ex.StackTrace}");
 		}
 	}
 
