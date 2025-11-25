fb64271759176d33b7f792e4fa929c9976dca675
Goodzy <bonkmenu@goodzy.dev>
2025-11-26 10:49:51 +1100

    Add complete encounter type support - shrines, altars, pots, and all interactables

--- BonkMenu.Features/WorldFeatures.cs
+++ BonkMenu.Features/WorldFeatures.cs
@@ -51,6 +51,137 @@ public static class WorldFeatures
 		}
 	}
 
+	private class EncounterPrefabCache
+	{
+		public GameObject chest;
+		public GameObject chestFree;
+		public GameObject greedAltar;
+		public GameObject chargeShrine;
+		public GameObject balanceShrine;
+		public GameObject moai;
+		public GameObject shadyGuy;
+		public GameObject microwave;
+		public GameObject pot;
+	}
+
+	private static EncounterPrefabCache ExtractAllPrefabs()
+	{
+		var cache = new EncounterPrefabCache();
+		
+		try
+		{
+			var allComponents = Object.FindObjectsOfType<MonoBehaviour>();
+			
+			// Extract from SpawnInteractables
+			foreach (var component in allComponents)
+			{
+				if (component.GetType().Name == "SpawnInteractables")
+				{
+					var chestField = component.GetType().GetField("chest");
+					var chestFreeField = component.GetType().GetField("chestFree");
+					
+					if (chestField != null)
+						cache.chest = chestField.GetValue(component) as GameObject;
+					if (chestFreeField != null)
+						cache.chestFree = chestFreeField.GetValue(component) as GameObject;
+					
+					MelonLogger.Msg("[WorldFeatures] Extracted chest prefabs from SpawnInteractables");
+				}
+				
+				// Extract from RandomObjectPlacer
+				if (component.GetType().Name == "RandomObjectPlacer")
+				{
+					var greedShrinesField = component.GetType().GetField("greedShrines");
+					var chargeShrinesField = component.GetType().GetField("chargeShrineSpawns");
+					var randomObjectsField = component.GetType().GetField("randomObjects");
+					
+					// Get greed shrine prefabs
+					if (greedShrinesField != null)
+					{
+						var greedShrineObj = greedShrinesField.GetValue(component);
+						if (greedShrineObj != null)
+						{
+							var prefabsField = greedShrineObj.GetType().GetField("prefabs");
+							if (prefabsField != null)
+							{
+								var prefabs = prefabsField.GetValue(greedShrineObj) as GameObject[];
+								if (prefabs != null && prefabs.Length > 0)
+									cache.greedAltar = prefabs[0];
+							}
+						}
+					}
+					
+					// Get charge shrine prefabs
+					if (chargeShrinesField != null)
+					{
+						var chargeShrineObj = chargeShrinesField.GetValue(component);
+						if (chargeShrineObj != null)
+						{
+							var prefabsField = chargeShrineObj.GetType().GetField("prefabs");
+							if (prefabsField != null)
+							{
+								var prefabs = prefabsField.GetValue(chargeShrineObj) as GameObject[];
+								if (prefabs != null && prefabs.Length > 0)
+									cache.chargeShrine = prefabs[0];
+							}
+						}
+					}
+					
+					// Get random objects (contains pots, shrines, altars, etc.)
+					if (randomObjectsField != null)
+					{
+						var randomObjects = randomObjectsField.GetValue(component) as Array;
+						if (randomObjects != null)
+						{
+							foreach (var randomObj in randomObjects)
+							{
+								if (randomObj == null) continue;
+								
+								var prefabsField = randomObj.GetType().GetField("prefabs");
+								if (prefabsField != null)
+								{
+									var prefabs = prefabsField.GetValue(randomObj) as GameObject[];
+									if (prefabs != null)
+									{
+										foreach (var prefab in prefabs)
+										{
+											if (prefab == null) continue;
+											
+											// Identify prefab by component type names
+											foreach (var comp in prefab.GetComponents<MonoBehaviour>())
+											{
+												var compType = comp.GetType().Name;
+												
+												if (cache.pot == null && compType == "InteractablePot")
+													cache.pot = prefab;
+												if (cache.shadyGuy == null && compType == "InteractableShadyGuy")
+													cache.shadyGuy = prefab;
+												if (cache.microwave == null && compType == "InteractableMicrowave")
+													cache.microwave = prefab;
+												if (cache.moai == null && compType == "InteractableShrineMoai")
+													cache.moai = prefab;
+												if (cache.balanceShrine == null && compType == "InteractableShrineBalance")
+													cache.balanceShrine = prefab;
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+					
+					MelonLogger.Msg("[WorldFeatures] Extracted prefabs from RandomObjectPlacer");
+				}
+			}
+		}
+		catch (Exception ex)
+		{
+			MelonLogger.Error($"[WorldFeatures] Error extracting prefabs: {ex.Message}");
+		}
+		
+		return cache;
+	}
+
 	public static void SpawnEncounter(int encounterId, string encounterName)
 	{
 		MelonLogger.Msg($"[WorldFeatures] Spawning encounter: {encounterName} (ID: {encounterId})");
@@ -67,100 +198,103 @@ public static class WorldFeatures
 			Vector3 spawnPosition = player.transform.position + player.transform.forward * 5f;
 			spawnPosition.y = player.transform.position.y;
 
-			// Try to find SpawnInteractables which holds prefab references
-			// This is more reliable than finding existing objects in scene
-			var spawners = Object.FindObjectsOfType<MonoBehaviour>();
-			GameObject chestPrefab = null;
-			GameObject chestFreePrefab = null;
-
-			// Search for SpawnInteractables component (it's in GlobalNamespace, so we search by field names)
-			foreach (var spawner in spawners)
-			{
-				if (spawner.GetType().Name == "SpawnInteractables")
-				{
-					// Use reflection to get the prefab fields
-					var chestField = spawner.GetType().GetField("chest");
-					var chestFreeField = spawner.GetType().GetField("chestFree");
-
-					if (chestField != null)
-						chestPrefab = chestField.GetValue(spawner) as GameObject;
-					if (chestFreeField != null)
-						chestFreePrefab = chestFreeField.GetValue(spawner) as GameObject;
-
-					if (chestPrefab != null || chestFreePrefab != null)
-					{
-						MelonLogger.Msg($"[WorldFeatures] Found SpawnInteractables with prefab references");
-						break;
-					}
-				}
-			}
-
+			// Extract all prefabs
+			var prefabs = ExtractAllPrefabs();
+			
 			// Spawn based on encounter type
 			GameObject spawnedObject = null;
+			GameObject prefabToSpawn = null;
+			string spawnName = "";
+
 			switch (encounterId)
 			{
+				case 2: // GreedAltar
+					prefabToSpawn = prefabs.greedAltar;
+					spawnName = "Greed Altar";
+					break;
+					
 				case 3: // ChestNormal
-					if ((Object)(object)chestPrefab != (Object)null)
+					prefabToSpawn = prefabs.chest;
+					spawnName = "Normal Chest";
+					if (prefabToSpawn != null)
 					{
-						spawnedObject = Object.Instantiate(chestPrefab, spawnPosition, Quaternion.identity);
-						var chestComponent = spawnedObject.GetComponent<InteractableChest>();
-						if ((Object)(object)chestComponent != (Object)null)
-						{
-							chestComponent.chestType = (EChest)0; // Normal chest
-						}
-						MelonLogger.Msg($"[WorldFeatures] Spawned normal chest at {spawnPosition}");
-					}
-					else
-					{
-						MelonLogger.Warning("[WorldFeatures] Normal chest prefab not found");
+						spawnedObject = Object.Instantiate(prefabToSpawn, spawnPosition, Quaternion.identity);
+						var chest = spawnedObject.GetComponent<InteractableChest>();
+						if (chest != null) chest.chestType = (EChest)0;
 					}
 					break;
 
 				case 4: // ChestFree
-					if ((Object)(object)chestFreePrefab != (Object)null)
+					prefabToSpawn = prefabs.chestFree;
+					spawnName = "Free Chest";
+					if (prefabToSpawn != null)
 					{
-						spawnedObject = Object.Instantiate(chestFreePrefab, spawnPosition, Quaternion.identity);
-						var chestComponent = spawnedObject.GetComponent<InteractableChest>();
-						if ((Object)(object)chestComponent != (Object)null)
-						{
-							chestComponent.chestType = (EChest)2; // Free chest
-						}
-						MelonLogger.Msg($"[WorldFeatures] Spawned free chest at {spawnPosition}");
-					}
-					else
-					{
-						MelonLogger.Warning("[WorldFeatures] Free chest prefab not found");
+						spawnedObject = Object.Instantiate(prefabToSpawn, spawnPosition, Quaternion.identity);
+						var chest = spawnedObject.GetComponent<InteractableChest>();
+						if (chest != null) chest.chestType = (EChest)2;
 					}
 					break;
 
-				case 5: // ChestEvil (Corrupt)
-					if ((Object)(object)chestPrefab != (Object)null)
+				case 5: // ChestEvil
+					prefabToSpawn = prefabs.chest;
+					spawnName = "Corrupt Chest";
+					if (prefabToSpawn != null)
 					{
-						spawnedObject = Object.Instantiate(chestPrefab, spawnPosition, Quaternion.identity);
-						var chestComponent = spawnedObject.GetComponent<InteractableChest>();
-						if ((Object)(object)chestComponent != (Object)null)
-						{
-							chestComponent.chestType = (EChest)1; // Corrupt chest
-						}
-						MelonLogger.Msg($"[WorldFeatures] Spawned corrupt chest at {spawnPosition}");
+						spawnedObject = Object.Instantiate(prefabToSpawn, spawnPosition, Quaternion.identity);
+						var chest = spawnedObject.GetComponent<InteractableChest>();
+						if (chest != null) chest.chestType = (EChest)1;
 					}
-					else
-					{
-						MelonLogger.Warning("[WorldFeatures] Corrupt chest prefab not found");
-					}
+					break;
+
+				case 6: // Moai
+					prefabToSpawn = prefabs.moai;
+					spawnName = "Moai";
+					break;
+
+				case 7: // ShadyGuy
+					prefabToSpawn = prefabs.shadyGuy;
+					spawnName = "Shady Guy";
+					break;
+
+				case 8: // BalanceShrine
+					prefabToSpawn = prefabs.balanceShrine;
+					spawnName = "Balance Shrine";
+					break;
+
+				case 9: // Microwave
+					prefabToSpawn = prefabs.microwave;
+					spawnName = "Microwave";
 					break;
 
 				default:
-					MelonLogger.Warning($"[WorldFeatures] Encounter type {encounterName} cannot be spawned as physical object yet");
-					// Fall back to UI encounter for unsupported types
+					MelonLogger.Warning($"[WorldFeatures] Encounter type {encounterName} triggers UI screen");
 					EncounterWindows encounterWindows = Object.FindObjectOfType<EncounterWindows>();
 					if ((Object)(object)encounterWindows != (Object)null)
 					{
-						EEncounter encounter = (EEncounter)encounterId;
-						encounterWindows.AddEncounter(encounter);
-						MelonLogger.Msg($"[WorldFeatures] Triggered UI encounter instead: {encounterName}");
+						encounterWindows.AddEncounter((EEncounter)encounterId);
+						MelonLogger.Msg($"[WorldFeatures] Triggered UI encounter: {encounterName}");
 					}
-					break;
+					return;
+			}
+
+			// Handle non-chest spawning (cases 2, 6, 7, 8, 9)
+			if (spawnedObject == null && prefabToSpawn != null)
+			{
+				spawnedObject = Object.Instantiate(prefabToSpawn, spawnPosition, Quaternion.identity);
+			}
+
+			if (spawnedObject != null)
+			{
+				MelonLogger.Msg($"[WorldFeatures] Successfully spawned {spawnName} at {spawnPosition}");
+			}
+			else
+			{
+				MelonLogger.Warning($"[WorldFeatures] Prefab not found for {spawnName} - falling back to UI");
+				EncounterWindows encounterWindows = Object.FindObjectOfType<EncounterWindows>();
+				if ((Object)(object)encounterWindows != (Object)null)
+				{
+					encounterWindows.AddEncounter((EEncounter)encounterId);
+				}
 			}
 		}
 		catch (Exception ex)
